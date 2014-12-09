e037c8b28450edb7a3b6166477c2fe4d27e26a66
Carlo <carlotheunissen96@gmail.com>
2014-12-09 13:46:16 +0100

    added startwebsite

---
+++ website/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="media/css/normalize.min.css" type="text/css" />
+        <link rel="stylesheet" href="media/css/main.css" type="text/css" />
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script type="text/javascript">
+        	if((typeof $) === "undefined"){
+        		document.write('<scr'+'ipt src="media/js/jquery-1.11.0.min.js"></sc'+'ript>')
+        	}
+        </script>
+        <script type="text/javascript" src="media/js/main.js"></script>
+    </head>
+    <body>
+    	
+    </body>
+</html>
