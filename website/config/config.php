7100968df97d6c14f6d332c99cf89b00d0256ece
Carlo <carlotheunissen96@gmail.com>
2014-12-15 21:09:25 +0100

    inlog pagina

--- website/config/config.php
+++ website/config/config.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
 setlocale(LC_MONETARY, 'nl_NL');
-$pdo = new PDO("mysql:host=localhost;dbname=webwinkel", "root", "");
+$pdo = new PDO("mysql:host=localhost;dbname=iris", "root", "");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
--- website/index.php
+++ website/index.php
@@ -3,7 +3,7 @@
     <head>
         <title></title>
         <link rel="stylesheet" href="media/css/normalize.min.css" type="text/css" />
-        <link rel="stylesheet" href="media/css/main.css" type="text/css" />
+        <link rel="stylesheet" href="media/css/index.css" type="text/css" />
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script type="text/javascript">
         	if((typeof $) === "undefined"){
@@ -11,8 +11,24 @@
         	}
         </script>
         <script type="text/javascript" src="media/js/main.js"></script>
+          <script type="text/javascript" src="media/js/cssrefresh.js"></script>
     </head>
     <body>
-    	
+    	<div id="container">
+               <div id="containerChar1" id="containerChar1">
+                    <div id="char1" class="characterAni"></div>
+               </div>
+               <div id="loginScreen">
+                    <div id="logo"></div>
+                   <form>
+                        <input type="text" placeholder="Naam" />
+                        <input type="password" placeholder="Wachtwoord">
+                        <input type="submit" value="Login" id="submit">
+                   </form>
+               </div>
+               <div id="containerChar2">
+                    <div id="char2" class="characterAni"></div>
+               </div>
+        </div>
     </body>
 </html>
